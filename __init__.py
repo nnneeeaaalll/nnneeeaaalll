51ef403d2f2ed9c56018a5cb0a0e5b8581d4d4c7
nnneeeaaalll <75534350+nnneeeaaalll@users.noreply.github.com>
2021-06-12 17:03:08 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+This is the program for Chapter 2 of the book
+'Celestial Calculations.'
+
+
+Copyright (c) 2018
+
+:author: J. L. Lawrence
+:version 3.0, 2018
+"""
+
+# No initialization is needed
+pass
